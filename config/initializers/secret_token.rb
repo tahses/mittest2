0a6ccf855c0e6e85480d03a52f5b05c583f4eb4c
Asfaw <asfaw@thetahses.com>
2013-05-18 05:10:35 -0600

    ts1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mitdiploma::Application.config.secret_key_base = '590ed9d16005c39a660ec3b86ff8322f317f9d9f351df212cc2e4e485e7bf0bb144e603c5db528a27e1024a428a7109da5124f4ba7b9a853bed80aac87242508'
